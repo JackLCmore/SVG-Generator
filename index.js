9f6eee6034a9a031e4c250be44e767b7f445dbf3
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-21 14:01:37 -0700

    added questions

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const shape = require('./library/shape.js');
 
 const questions = [
     {
         type:"",
-        name:"",
-        message:"",
+        name:"text",
+        message:"What text would you like your logo to include(3 char max)",
     },
     {
-        type:"",
-        name:"",
-        message:"",
+        type:"input",
+        name:"textColor",
+        message:"What color would you like the text of your logo to be?",
     },
+    {
+        type:"input",
+        name:"color",
+        message:"What color would you like your logo background to be?",
+    },
+    {
+        type:"list",
+        name:"shape",
+        message:"Which Shape would you like your logo to be?",
+        choices:["Triangle","Circle","Square"]
+    }
 ]
---
+++ library/shape.js
@@ -0,0 +1,25 @@
+class Shape {
+    constructor(color, text, textColor){
+        this.color = color;
+        this.text = text;
+        this.text = textColor;
+    }
+}
+
+class Triangle extends Shape {
+    constructor(){
+        super(color, text, textColor);
+    }
+}
+
+class Circle extends Shape {
+    constructor(){
+        super(color, text, textColor);
+    }
+}
+
+class Square extends Shape {
+    constructor(){
+        super(color, text, textColor);
+    }
+}
