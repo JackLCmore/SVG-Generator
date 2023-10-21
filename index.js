30cf34ac4c9da00b8fe64c13df700eb98f5e8123
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-21 13:42:43 -0700

    added all base files, installed npm packages

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type:"",
+        name:"",
+        message:"",
+    },
+    {
+        type:"",
+        name:"",
+        message:"",
+    },
+]
