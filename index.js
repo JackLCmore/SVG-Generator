5a27e34b01f5a60eee329bac3c69627069cc7816
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-21 18:56:40 -0700

    generator fully functional

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shape = require('./library/shape.js');
+const Shape = require('./library/shape.js')
+// let Triangle = require('./library/shape.js');
+// let Square = require('./library/shape.js');
+// let Circle = require('./library/shape.js');
+class Triangle extends Shape {
+    constructor(color,text,textColor){
+        super(color,text,textColor)
+        this.element = `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />
+
+        <text x="150" y="150" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+    }
+}
+
+class Circle extends Shape {
+    constructor(color,text,textColor){
+        super(color,text,textColor)
+        this.element = `<circle cx="150" cy="100" r="80" fill="${this.color}"/>
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+    }
+}
+
+class Square extends Shape {
+    constructor(color,text,textColor){
+        super(color,text,textColor)
+        this.element = `<rect width="150" height="150" fill="${this.color}"/>
+
+        <text x="75" y="100" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+    }
+}
 
 const questions = [
     {
@@ -16,13 +45,13 @@ const questions = [
     },
     {
         type:"input",
-        name:"textColor",
-        message:"What color would you like the text of your logo to be?(color key or hexcode)",
+        name:"color",
+        message:"What color would you like your logo background to be?(color key or hexcode)",
     },
     {
         type:"input",
-        name:"color",
-        message:"What color would you like your logo background to be?(color key or hexcode)",
+        name:"textColor",
+        message:"What color would you like the text of your logo to be?(color key or hexcode)",
     },
     {
         type:"list",
@@ -32,14 +61,32 @@ const questions = [
     }
 ]
 
+function determineShape(data){
+    if (data.shape === "Triangle"){
+        const newTri = new Triangle(data.color , data.text , data.textColor)
+        return newTri.element;
+    }
+    else if (data.shape === "Circle"){
+        const newCirc = new Circle(data.color , data.text , data.textColor)
+        return newCirc.element;
+    }
+    else if (data.shape === "Square"){
+        const newSquare = new Square(data.color , data.text , data.textColor)
+        return newSquare.element;
+    }
+}
+
 function generateSVG(data){
-    return ``;
+    return `<svg width="300" height="200">
+    ${determineShape(data)}
+    </svg>`;
 };
 
 function init (){
     inquirer
     .prompt(questions)
     .then((response)=>{
+        console.log(response)
         fs.writeFile("./destination/logo.svg",generateSVG(response),(err)=>
         err ? console.error(err) : console.log("Success"))
     });    
--- library/shape.js
+++ library/shape.js
@@ -2,24 +2,41 @@ class Shape {
     constructor(color, text, textColor){
         this.color = color;
         this.text = text;
-        this.text = textColor;
+        this.textColor = textColor;
     }
 }
 
-class Triangle extends Shape {
-    constructor(){
-        super(color, text, textColor);
-    }
-}
+// class Triangle extends Shape {
+//     constructor(color,text,textColor){
+//     }
+//     generateTriangle(){
+//         return`<rect width="150" height="150" fill="${this.color}"/>
 
-class Circle extends Shape {
-    constructor(){
-        super(color, text, textColor);
-    }
-}
+//         <text x="75" y="100" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+//     }
+// }
 
-class Square extends Shape {
-    constructor(){
-        super(color, text, textColor);
-    }
-}
+// class Circle extends Shape {
+//     constructor(color,text,textColor){
+//     }
+//     generateCircle(){
+//         return`<rect width="150" height="150" fill="${this.color}"/>
+
+//         <text x="75" y="100" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+//     }
+// }
+
+// class Square extends Shape {
+//     constructor(color,text,textColor){
+//     }
+//     generateSquare(){
+//         return`<rect width="150" height="150" fill="${this.color}"/>
+
+//         <text x="75" y="100" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>`
+//     }
+// }
+
+module.exports = Shape;
+// module.exports = Triangle;
+// module.exports = Circle;
+// module.exports = Square;
