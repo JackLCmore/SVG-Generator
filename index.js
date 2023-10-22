2a4d01af901dacc6467a42525d31a12410601c35
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-21 17:04:53 -0700

    added svg gen functionality, testing svg properties

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const shape = require('./library/shape.js');
 
 const questions = [
     {
-        type:"",
+        type:"input",
         name:"text",
-        message:"What text would you like your logo to include(3 char max)",
+        message:"What text would you like your logo to include?(3 char max)",
+        validate: async (input) =>{
+            if(input.length > 3){
+                return "Please input 3 characters or less"
+            }
+            return true;
+        }
     },
     {
         type:"input",
         name:"textColor",
-        message:"What color would you like the text of your logo to be?",
+        message:"What color would you like the text of your logo to be?(color key or hexcode)",
     },
     {
         type:"input",
         name:"color",
-        message:"What color would you like your logo background to be?",
+        message:"What color would you like your logo background to be?(color key or hexcode)",
     },
     {
         type:"list",
@@ -25,3 +31,18 @@ const questions = [
         choices:["Triangle","Circle","Square"]
     }
 ]
+
+function generateSVG(data){
+    return ``;
+};
+
+function init (){
+    inquirer
+    .prompt(questions)
+    .then((response)=>{
+        fs.writeFile("./destination/logo.svg",generateSVG(response),(err)=>
+        err ? console.error(err) : console.log("Success"))
+    });    
+}
+
+init();
